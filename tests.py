0e1b285ea3d450b06216154f0acb076b3b81274a
Josephine Schertler <schertler@cip.ifi.lmu.de>
2024-07-31 12:17:35 +0200

    fixed edge case testing

--- tests.py
+++ tests.py
@@ -26,6 +26,6 @@ def test_empty_result():
     assert report.result == ""
 
 def test_edgecase_date():
-    with pytest.raises(ValueError):
-        Report("Emilia", "49.08.2002", "pass", "Description", "Result")
-
+    # testing leap year
+    report = Report("Emilia", "29.2.2024", "pass", "Description", "Result")
+    assert report.date == "2024-02-29"
